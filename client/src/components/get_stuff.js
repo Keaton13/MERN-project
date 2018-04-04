ac5cab337bffb8323df1f43dbecbbd4716da8439
Keaton Krieger <bajabuggy313@gmail.com>
2018-04-04 16:20:52 -0700

    Mern Stack

--- client/src/components/get_stuff.js
+++ client/src/components/get_stuff.js
@@ -2,9 +2,14 @@ import React, { Component } from 'react';
 import axios from 'axios';
 class GetStuff extends Component {
     componentDidMount(){
-        axios.get('http://localhost:9000/api/get-stuff').then( resp => {
-            console.log('Responce from server:', resp);
+        axios.get('/api/get-stuff').then( resp => {
+            console.log('Response from server:', resp);
         });
+
+        axios.post('/api/get-user').then( resp => {
+            console.log('Response from get user:', resp);
+        })
+
     }
     render(){
         return(
--- index.js
+++ index.js
@@ -1,19 +1,38 @@
-// Require needed modules / dependencies
 const express = require('express');
 const cors = require('cors');
+const { resolve } = require('path');
 const port = process.env.port || 9000;
 
 const app = express();
 
 app.use(cors());
 
-app.get('/api/get-stuff', (req, res) => {
+app.use(express.static(resolve(__dirname, 'client', 'dist')));
+
+function log(msg) {
+    return (req, res, next) => {
+        console.log(msg);
+        next();
+    }
+
+}
+app.use('/api/get-stuff', log('This is middleware route'));
+
+app.get('/api/get-stuff', log, (req, res) => {
    res.send({success: true, message: 'Here is some stuff from the back-end'});
 });
 
-app.get('/', (req, res) => {
-   res.send('<h1> YO </h1>')
+app.post('/api/get-user', (req, res) => {
+   res.send({success: true, user: {name: 'Jim Bob', age: 57}});
 });
+
+app.get('*', (req, res) => {
+    res.sendFile(resolve(__dirname, 'client', 'dist', 'index.html'));
+});
+
+// app.get('/', (req, res) => {
+//     res.send('<h1> YO </h1>')
+// });
 
 app.listen(port ,() => {
     console.log("server running on port " + port);
